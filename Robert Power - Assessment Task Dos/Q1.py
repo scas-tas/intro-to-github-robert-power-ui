2dd523c5bff7639ca6b03bd17874d5ebb89e9d16
robert-power-ui <robert-power@scas.nsw.edu.au>
2026-07-25 14:08:37 +1000

    ex
    h

--- Robert Power - Assessment Task Dos/Q1.py
+++ Robert Power - Assessment Task Dos/Q1.py
@@ -1,13 +1,15 @@
 def combine_trucks(trucks, first_truck, second_truck):
     return(int(trucks[first_truck-1] + trucks[second_truck-1]))
-    # TODO: return the total packages in first_truck and second_truck
+    #returns the total packages in first_truck and second_truck
     pass
 
 
 def main():
+    #lists the number of trucks
     trucks = [4, 7, 2, 6, 9]
-    print(combine_trucks(trucks, 2, 4))  # Expected: 13
-    print(combine_trucks(trucks, 1, 3))  # Expected: 6
+    #prints the combined number of packages within the two trucks chosen
+    print(combine_trucks(trucks, 4, 3))  # Expected: 13
+    print(combine_trucks(trucks, 3, 5))  # Expected: 6
 
 
 main()

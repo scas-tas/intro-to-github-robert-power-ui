c7973e04c44d39956c61601a0b266dbe239256b6
robert-power-ui <robert-power@scas.nsw.edu.au>
2026-07-29 18:17:41 +1000

    Update Q3.py

--- Robert Power - Assessment Task Dos/Q3.py
+++ Robert Power - Assessment Task Dos/Q3.py
@@ -1,16 +1,28 @@
 def add_item(inventory: dict, name: str, quantity: int) -> None:
-    inventory[name] = quantity
+    if name in inv:
+        inventory[name] = inventory[name] + quantity
+    else:
+        inventory[name] = quantity
     print(inv)
     pass
  
 def remove_item(inventory: dict, name: str, quantity: int) -> None:
+    if name in inv:
+        inventory[name] = inventory[name] - quantity
+        if inventory[name] <= 0:
+            inventory[name] = 0
+    else:
+        pass
+    print(inv)
     pass
  
 def get_stock_report(inventory: dict) -> str:
     pass
 
 
-inv = {}
+inv = {'apples': 5,
+       'mangos': 4129,
+       'bananas': 150}
 add_item(inv, 'apples', 10)
-add_item(inv, 'bananas', 5)
+remove_item(inv, 'bananas', 5)
 get_stock_report(inv)
